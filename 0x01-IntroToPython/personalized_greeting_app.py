d16848f16027fdc30cac6b77454454d498dfcc6d
Olayiwola Sulaimon <osulaimon19@yahoo.com>
2024-11-01 12:53:50 +0100

    Implement Greeting App with core features

---
+++ 0x01-IntroToPython/personalized_greeting_app.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+name = input("Enter your name: ")
+favourite_color = input("Enter your favorite color: ")
+
+greeting = f"Hello, {name}! Your favorite color, {favourite_color}, is awesome!"
+
+print(greeting)
